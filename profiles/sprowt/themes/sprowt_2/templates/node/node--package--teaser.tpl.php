6253b546d7830be3812b85c09cf927ca22c56daa
Kyle Hofmeyer <kyle@kylehofmeyer.com>
2016-12-02 13:30:19 -0500

    Adding highlight package styling and template files

--- profiles/sprowt/themes/sprowt_2/templates/node/node--package--teaser.tpl.php
+++ profiles/sprowt/themes/sprowt_2/templates/node/node--package--teaser.tpl.php
@@ -1,16 +1,32 @@
-<article<?php print $attributes; ?>>
-
-  <div class="icon-wrap">
-    <?php print render($content['field_icon']); ?>
-    <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h3>
-  </div>
-  <div<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
+<?php
+  $highlighted = FALSE;
+  if ($content['field_highlight'][0]['#markup'] == 'Yes') {
+    $highlighted = TRUE;
+  };
+?>
+<?php if($highlighted): ?>
+  <div class="node-wrap highlighted">
+<?php else: ?>
+  <div class="node-wrap">
+<?php endif; ?>
+  <div<?php print $attributes; ?>>
+    <div class="icon-wrap">
+      <?php print render($content['field_icon']); ?>
+    </div>
+    <div class="content-wrap">
+      <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h3>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['field_highlight']);
+        hide($content['field_highlight_icon']);
+        print render($content);
+      ?>
+    </div>
+    <?php if($highlighted) {
+        print render($content['field_highlight_icon']);
+      };
     ?>
   </div>
-
-</article>
+</div>
