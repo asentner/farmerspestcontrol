f7b7a0919aaa79ddff8f4ccdd6f865c4e9f44974
Will <littlmac0@gmail.com>
2016-12-21 10:52:35 -0500

    bug fixes

--- profiles/sprowt/background_sprowt_build.php
+++ profiles/sprowt/background_sprowt_build.php
@@ -5,6 +5,7 @@ ignore_user_abort();
 
 if(!defined('DRUPAL_ROOT')){
     define('DRUPAL_ROOT', str_replace("/profiles/sprowt", "", getcwd()));
+    chdir(DRUPAL_ROOT);
     require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
     drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 }
