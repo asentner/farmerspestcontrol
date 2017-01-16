9fb63db547b8e860c384849e0e4a1694ec51f763
Will McMillian <wmcmillian@coalmarch.com>
2017-01-16 10:55:37 -0500

    fix service menu block

--- profiles/sprowt/includes/blockbuilder.php
+++ profiles/sprowt/includes/blockbuilder.php
@@ -152,19 +152,25 @@ class BlockBuilder {
             switch ($row['module']) {
                 case 'block':
                     $row['delta'] = $this->block_custom_map[$current_row['delta']];
-                    $row['css_class'] .= ' sprowt-block-imported';
+                    if(strpos($row['css_class'], 'sprowt-block-imported') === false) {
+                      $row['css_class'] .= ' sprowt-block-imported';
+                    }
                     break;
                 case 'multiblock':
                     $row['delta'] = $this->multiblock_map[$current_row['delta']];
-                    $row['css_class'] .= ' sprowt-block-imported';
+                    if(strpos($row['css_class'], 'sprowt-block-imported') === false) {
+                      $row['css_class'] .= ' sprowt-block-imported';
+                    }
                     break;
                 case 'sprowt_block_cta':
                     $nid = str_replace('sprowt_block_cta_', '', $row['delta']);
                     $uuid = empty($this->uuid_map[$nid]) ? 0 : $this->uuid_map[$nid];
                     if (!empty($nid_map[$uuid])) {
-                        $row['css_class'] .= ' sprowt-block-imported block--sprowt-block-cta-sprowt-block-cta-' . $nid;
+                        if(strpos($row['css_class'], 'sprowt-block-imported') === false) {
+                          $row['css_class'] .= ' sprowt-block-imported';
+                        }
+                        $row['css_class'] .= ' block--sprowt-block-cta-sprowt-block-cta-' . $nid;
                         $row['delta'] = 'sprowt_block_cta_' . $nid_map[$uuid];
-
                     } else {
                         $row['delta'] = '';
                     }
