48ae4b4b549942b13b1d72c7b65bf5c1031b9e62
David White <dwhite@dwhite.local>
2017-03-17 11:25:13 -0400

    remove header tag from cta template in all themes

--- profiles/sprowt/themes/sprowt_2/templates/node/node--cta--teaser.tpl.php
+++ profiles/sprowt/themes/sprowt_2/templates/node/node--cta--teaser.tpl.php
@@ -1,9 +1,7 @@
 <article<?php print $attributes; ?>>
 
   <div<?php print $content_attributes; ?>>
-    <header>
-      <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
-    </header>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 
     <?php
       // We hide the comments and links now so that we can render them later.
