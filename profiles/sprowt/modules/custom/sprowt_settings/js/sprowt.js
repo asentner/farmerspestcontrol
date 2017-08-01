66b93a00dbde5337a801fd87ac1e260b1da6a654
Will McMillian <wmcmillian@coalmarch.com>
2017-08-01 12:12:48 -0400

    Added utm script

--- profiles/sprowt/modules/custom/sprowt_settings/js/sprowt.js
+++ profiles/sprowt/modules/custom/sprowt_settings/js/sprowt.js
@@ -8,6 +8,23 @@
                 script.attr('src', '//' + Drupal.settings.sprowt.ctmAccountId + '.tctm.co/t.js');
                 script.insertBefore(firstScript);
             }
+
+            //gtm parameter fields
+            var utm = {
+                utm_source: $('#edit-submitted-utm-source'),
+                utm_medium: $('#edit-submitted-utm-medium'),
+                utm_campaign: $('#edit-submitted-utm-campaign'),
+                utm_content: $('#edit-submitted-utm-content')
+            }
+
+            $.each(utm, function(key, $field){
+                if($field.length > 0) {
+                    var params = new URLSearchParams(window.location.search);
+                    if(null !== params.get(key)) {
+                        $field.val(params.get(key));
+                    }
+                }
+            });
         }
     }
 }(jQuery);
