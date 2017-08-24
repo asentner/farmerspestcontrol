333515ce32a39287d07f95fa1108a77cb9ae9d97
Will McMillian <wmcmillian@coalmarch.com>
2017-08-24 10:16:35 -0400

    added new module for fixing that damn reset button

---
+++ profiles/sprowt/modules/custom/views_ajax_reset_fix/views_ajax_reset_fix.js
@@ -0,0 +1,16 @@
+(function($){
+  Drupal.behaviors.viewsAjaxResetFix = {
+    attach: function (context, settings) {
+      $('.views-ajax-reset-fix-button').once('views-ajax-reset-fix', function(){
+          $(this).bind('click', function(e){
+            e.preventDefault();
+            var $form = $(this.form);
+            $form.clearForm();
+            $form.find('.views-ajax-reset-fix-button-submit').trigger('click');
+            return false;
+          });
+      });
+      
+    }
+  };
+})(jQuery);
