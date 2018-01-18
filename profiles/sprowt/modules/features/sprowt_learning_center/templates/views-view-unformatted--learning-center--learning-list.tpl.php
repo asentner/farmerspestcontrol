e5ad95cbb7b39c8de6549cc363321cc5c30763ea
Will <littlmac0@gmail.com>
2018-01-18 17:02:31 -0500

    update template

--- profiles/sprowt/modules/features/sprowt_learning_center/templates/views-view-unformatted--learning-center--learning-list.tpl.php
+++ profiles/sprowt/modules/features/sprowt_learning_center/templates/views-view-unformatted--learning-center--learning-list.tpl.php
@@ -7,11 +7,17 @@
  * @ingroup views_templates
  */
 ?>
-<?php if (!empty($title)): ?>
-  <?php print $title; ?>
-<?php endif; ?>
-<?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-    <?php print $row; ?>
-  </div>
-<?php endforeach; ?>
+<div class="views-row">
+    <?php if (!empty($title)): ?>
+        <div class="header">
+            <?php print $title; ?>
+        </div>
+    <?php endif; ?>
+    <div class="issue-wrap">
+        <?php foreach ($rows as $id => $row): ?>
+            <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+                <?php print $row; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
