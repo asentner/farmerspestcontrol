0e64cea2396e61e8bf308ad0ad9f08a0a72e226b
Will McMillian <wmcmillian@coalmarch.com>
2018-06-12 15:10:38 -0400

    async background request for sprowtbuilder

---
+++ profiles/sprowt/BackgroundBuilder.php
@@ -0,0 +1,112 @@
+<?php
+
+set_time_limit(0);
+ini_set('max_execution_time', 0); //0=NOLIMIT
+ignore_user_abort();
+
+if(!defined('DRUPAL_ROOT')){
+    define('DRUPAL_ROOT', str_replace("/profiles/sprowt", "", getcwd()));
+    chdir(DRUPAL_ROOT);
+    require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+    drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+}
+
+//set profile because it's not set for some reason
+
+variable_set('install_profile', 'sprowt');
+
+if(!db_table_exists('sprowt_progress')){
+    $sprowt_progress_schema = array(
+        'description' => 'The initial setup for this sprowt site',
+        'fields' => array(
+            'id' => array('type' => 'serial', 'unsigned' => true, 'not null' => true),
+            'function' => array('type'=> 'varchar', 'length' => 255, 'not null' => true, 'default' => ''),
+            'progress' => array('type'=> 'int', 'default' => '0'),
+            'message' =>  array('type'=> 'text'),
+            'actions' => array('type'=> 'int', 'default' => '0'),
+            'current_action' => array('type'=> 'int', 'default' => '0'),
+            'count' => array('type'=> 'int', 'default' => '0'),
+            'complete' => array('type'=> 'int','not null' => true, 'default' => '0'),
+        ),
+        'primary key' => array('id')
+    );
+    
+    db_create_table('sprowt_progress', $sprowt_progress_schema);
+    
+    global $base_url;
+    $progress_id = db_insert('sprowt_progress')
+        ->fields(array(
+            'function' => 'init',
+            'progress' => 0
+        ))
+        ->execute();
+}
+
+$progress = db_query("SELECT * FROM sprowt_progress")->fetchAssoc();
+
+
+function update_progress($function, $perc, $message, $actions, $current, $id) {
+    $updated = db_update('sprowt_progress') // Table name no longer needs {}
+    ->fields(array(
+        'function' => $function,
+        'progress' => floor($perc * 100),
+        'message' => $message,
+        'actions' => $actions,
+        'current_action' => $current
+    ))
+        ->condition('id', $id, '=');
+    
+    $updated->execute();
+}
+
+$sb = new SprowtBuilder();
+$data = $sb->getData();
+
+
+$actions = array(
+    'getData' => "Getting Sprowt Data...",
+    'addUsers' => "Adding Users...",
+    'addNodes' => "Adding Default Nodes...",
+    'addCompanyInfo' => "Adding Company Info...",
+    'addReview' => "Adding Review Sites...",
+    'addMarketsServices' => "Adding Markets and Services...",
+    'addLocations' => "Adding Locations...",
+    'addBranding' => "Adding Branding...",
+    'addSocial' => "Adding Social Media...",
+    'addIntegrations' => "Adding Integrations...",
+    'revertFeatures' => "Revert Features...",
+    'addNodeDefaultImages' => "Updating Default Images..."
+);
+
+if(!empty($data['starter']['is_starter'])) {
+    $actions['setUpSprowtStarter'] = 'Setting Up Sprowt Starter Specific Settings...';
+}
+
+$i = 0;
+
+foreach($actions as $method => $message) {
+    if(empty($i)) {
+        update_progress($method, 0 , $message, count($actions), $i, $progress['id']);
+    }
+    else {
+        update_progress($method, $i/count($actions) , $message, count($actions), $i, $progress['id']);
+    }
+    $sb->$method();
+    $i++;
+}
+
+
+$updated = db_update('sprowt_progress') // Table name no longer needs {}
+->fields(array(
+    'function' => 'Done',
+    'progress' => 100,
+    'message' => 'Finished!',
+    'complete' => 1
+))
+    ->condition('id', $progress['id'], '=')
+    ->execute();
+
+echo json_encode(array(
+    'success' => true,
+    'message' => 'Sprowt Setup Done!'
+));
--- profiles/sprowt/background_sprowt_build.php
+++ profiles/sprowt/background_sprowt_build.php
@@ -1,112 +1,55 @@
 <?php
 
-set_time_limit(0);
-ini_set('max_execution_time', 0); //0=NOLIMIT
-ignore_user_abort();
-
-if(!defined('DRUPAL_ROOT')){
-    define('DRUPAL_ROOT', str_replace("/profiles/sprowt", "", getcwd()));
-    chdir(DRUPAL_ROOT);
-    require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-    drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-}
-
-//set profile because it's not set for some reason
-
-variable_set('install_profile', 'sprowt');
-
-if(!db_table_exists('sprowt_progress')){
-    $sprowt_progress_schema = array(
-      'description' => 'The initial setup for this sprowt site',
-      'fields' => array(
-        'id' => array('type' => 'serial', 'unsigned' => true, 'not null' => true),
-        'function' => array('type'=> 'varchar', 'length' => 255, 'not null' => true, 'default' => ''),
-        'progress' => array('type'=> 'int', 'default' => '0'),
-        'message' =>  array('type'=> 'text'),
-        'actions' => array('type'=> 'int', 'default' => '0'),
-          'current_action' => array('type'=> 'int', 'default' => '0'),
-          'count' => array('type'=> 'int', 'default' => '0'),
-        'complete' => array('type'=> 'int','not null' => true, 'default' => '0'),
-      ),
-      'primary key' => array('id')
-    );
+function curl_request_async($url, $params, $type='POST')
+{
+    $post_params = [];
     
-    db_create_table('sprowt_progress', $sprowt_progress_schema);
+    foreach ($params as $key => &$val) {
+        if (is_array($val)) $val = implode(',', $val);
+        $post_params[] = $key.'='.rawurlencode($val);
+    }
+    $post_string = implode('&', $post_params);
     
-    global $base_url;
-    $progress_id = db_insert('sprowt_progress')
-        ->fields(array(
-            'function' => 'init',
-            'progress' => 0
-        ))
-        ->execute();
-}
-
-$progress = db_query("SELECT * FROM sprowt_progress")->fetchAssoc();
-
-
-function update_progress($function, $perc, $message, $actions, $current, $id) {
-    $updated = db_update('sprowt_progress') // Table name no longer needs {}
-      ->fields(array(
-        'function' => $function,
-        'progress' => floor($perc * 100),
-        'message' => $message,
-        'actions' => $actions,
-        'current_action' => $current
-      ))
-      ->condition('id', $id, '=');
-      
-    $updated->execute();
-}
-
-$sb = new SprowtBuilder();
-$data = $sb->getData();
-
-
-$actions = array(
-    'getData' => "Getting Sprowt Data...",
-    'addUsers' => "Adding Users...",
-    'addNodes' => "Adding Default Nodes...",
-    'addCompanyInfo' => "Adding Company Info...",
-    'addReview' => "Adding Review Sites...",
-    'addMarketsServices' => "Adding Markets and Services...",
-    'addLocations' => "Adding Locations...",
-    'addBranding' => "Adding Branding...",
-    'addSocial' => "Adding Social Media...",
-    'addIntegrations' => "Adding Integrations...",
-    'revertFeatures' => "Revert Features...",
-    'addNodeDefaultImages' => "Updating Default Images..."
-);
-
-if(!empty($data['starter']['is_starter'])) {
-    $actions['setUpSprowtStarter'] = 'Setting Up Sprowt Starter Specific Settings...';
-}
-
-$i = 0;
-
-foreach($actions as $method => $message) {
-    if(empty($i)) {
-        update_progress($method, 0 , $message, count($actions), $i, $progress['id']);
+    $parts = parse_url($url);
+    
+    if(empty($parts['path'])) {
+        $parts['path'] = '/';
     }
-    else {
-        update_progress($method, $i/count($actions) , $message, count($actions), $i, $progress['id']);
+    
+    if($parts['scheme'] == 'https') {
+        $parts['port'] = 443;
+        $parts['host'] = "ssl://{$parts['host']}";
     }
-    $sb->$method();
-    $i++;
+    
+    $fp = fsockopen($parts['host'],
+        isset($parts['port']) ? $parts['port'] : 80,
+        $errno, $errstr, 30);
+    
+    // Data goes in the path for a GET request
+    if('GET' == $type) $parts['path'] .= '?'.$post_string;
+    
+    $out = "$type ".$parts['path']." HTTP/1.1\r\n";
+    $out.= "Host: ".$parts['host']."\r\n";
+    $out.= "Content-Type: application/x-www-form-urlencoded\r\n";
+    $out.= "Content-Length: ".strlen($post_string)."\r\n";
+    $out.= "Connection: Close\r\n\r\n";
+    // Data goes in the request body for a POST request
+    if ('POST' == $type && isset($post_string)) $out.= $post_string;
+    
+    fwrite($fp, $out);
+    fclose($fp);
 }
 
+$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+$base = str_replace(basename($actual_link), '', $actual_link);
+$url = $base . 'BackgroundBuilder.php';
+if(!empty($_GET['XDEBUG_SESSION_START'])) {
+    $url .= '?XDEBUG_SESSION_START=1';
+}
 
-$updated = db_update('sprowt_progress') // Table name no longer needs {}
-  ->fields(array(
-    'function' => 'Done',
-    'progress' => 100,
-    'message' => 'Finished!',
-    'complete' => 1
-  ))
-  ->condition('id', $progress['id'], '=')
-  ->execute();
-  
+curl_request_async($url, []);
+
 echo json_encode(array(
     'success' => true,
-    'message' => 'Sprowt Setup Done!'
+    'message' => 'Sprowt Setup Started!'
 ));
