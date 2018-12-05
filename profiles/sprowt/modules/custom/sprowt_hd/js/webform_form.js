d751c2b5088865fba591297f92aaad0c5e59e924
Will <littlmac0@gmail.com>
2018-12-05 13:01:49 -0500

    update speedlead to use custom form reactor fields

---
+++ profiles/sprowt/modules/custom/sprowt_hd/js/webform_form.js
@@ -0,0 +1,59 @@
+(function($){
+    var addCustomField = function(fieldId, field) {
+        if(fieldId) {
+            var id = fieldId.toLowerCase().replace(/[^a-z-]/, '-');
+        }
+        else {
+            id = Math.random()
+        }
+        var $fieldset = $($('#customFieldTemplate').html());
+        $fieldset.attr('id', id);
+        $fieldset.find('.field-id').val(fieldId);
+        $fieldset.find('.field-field').val(field);
+        $('#customFieldsDisplay').append($fieldset);
+    }
+
+    var updateCustomFields = function(){
+        var customfields = [];
+        $('.custom-field-fieldset').each(function(){
+            customfields.push({
+                fieldId: $(this).find('.field-id').val(),
+                field: $(this).find('.field-field').val(),
+            });
+        });
+        $('#customFields').val(JSON.stringify(customfields));
+    };
+
+    $(document).on('click', '#addCustomField', function(e){
+        e.preventDefault();
+        addCustomField(null, null);
+    });
+
+    $(document).on('click', '.field-remove', function(e) {
+        var $fieldset = $(this).closest('.custom-field-fieldset');
+        $fieldset.remove();
+        updateCustomFields();
+    });
+
+    $(document).on('keyup', '.field-id', function(){
+        updateCustomFields();
+    });
+
+    $(document).on('change', '.field-field', function(){
+        updateCustomFields();
+    });
+
+    $(document).ready(function(){
+        var customFields = $('#customFields').val();
+        if(customFields) {
+            customFields = JSON.parse(customFields);
+        }
+
+        if(customFields) {
+            $.each(customFields, function(i,v) {
+                addCustomField(v.fieldId, v.field);
+            });
+        }
+    });
+
+})(jQuery)
