5d43ddcccd242fdfba285027ac3e177ce7f4055c
Will <littlmac0@gmail.com>
2019-01-23 10:17:34 -0500

    add privacy policy if it's not created in initial node import

--- profiles/sprowt/sprowtbuilder.php
+++ profiles/sprowt/sprowtbuilder.php
@@ -333,6 +333,11 @@ Class SprowtBuilder {
         variable_set('webform_default_from_address', $this->data['company_info']['webform_from_email']);
         variable_set('webform_default_from_name', $this->data['company_info']['webform_from_name']);
         variable_set('webform_email_replyto', false);
+
+        $privacy = entity_uuid_load('node', '6da1b988-5564-47a2-8404-72ec6aaa46ed');
+        if(empty($privacy)) {
+            $this->importPrivacyPolicy();
+        }
     }
     
     function addDefaultImages() {
