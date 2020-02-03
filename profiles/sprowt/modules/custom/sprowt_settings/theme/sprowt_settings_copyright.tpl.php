4b1da1616590b4d117dc85b69509a66e725e022b
Will McMillian <wmcmillian@coalmarch.com>
2020-02-03 13:25:37 -0500

    update default copyright in footer

--- profiles/sprowt/modules/custom/sprowt_settings/theme/sprowt_settings_copyright.tpl.php
+++ profiles/sprowt/modules/custom/sprowt_settings/theme/sprowt_settings_copyright.tpl.php
@@ -1,18 +1,2 @@
-<?php
-    switch(variable_get('sprowt_settings_company_type')) {
-        case 'pest_control':
-            $intro = 'Pest Control Marketing +';
-            break;
-        case 'lawn_care':
-            $intro = 'Lawn Care Websites +';
-            break;
-        case 'home_services':
-            $intro = 'Home Services +';
-            break;
-        default:
-            $intro = '<a href="http://www.getsprowt.com" target="_blank">Sprowt<span>&trade;</span></a> website design and Drupal development by';
-    }
-?>
-<p>&copy; [current-year] [company-name]. All rights reserved.
-	<?php print $intro; ?> <a href="http://www.coalmarch.com" target="_blank">Coalmarch Productions, Raleigh NC</a></p>
+<p>&copy; [current-year] [company-name]. All rights reserved. Powered by <a href="https://www.coalmarch.com" target="_blank">Coalmarch</a></p>
 <p><a href="/privacy-policy">Privacy Policy</a></p>
