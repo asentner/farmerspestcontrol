fc4e95c48b140de16c9bc9d8e38a779f2c4508f1
Matthan Heiselt <mheiselt@coalmarch.com>
2020-03-06 10:00:35 -0500

    moved solution page contact lock-form styling/js from theme to feature

--- profiles/sprowt/modules/features/sprowt_solution_finder/js/solutions_pages.js
+++ profiles/sprowt/modules/features/sprowt_solution_finder/js/solutions_pages.js
@@ -1,18 +1,48 @@
 (function ($) {
-  
-$(document).ready(function(){
-  var $solutionMenuToggleBlock = $('.solution-menu-toggle');
 
-  if($solutionMenuToggleBlock.length > 0) {
-    var $menu = $('.block--nav-main');
-    var $toggle = $solutionMenuToggleBlock.find('.menu-toggle');
+  $(document).ready(function () {
+    var $solutionMenuToggleBlock = $('.solution-menu-toggle');
 
-    $toggle.click(function(e){
-      e.preventDefault();
-      $toggle.toggleClass('open');
-      $menu.toggleClass('open').slideToggle();
+    if ($solutionMenuToggleBlock.length > 0) {
+      var $menu = $('.block--nav-main');
+      var $toggle = $solutionMenuToggleBlock.find('.menu-toggle');
+
+      $toggle.click(function (e) {
+        e.preventDefault();
+        $toggle.toggleClass('open');
+        $menu.toggleClass('open').slideToggle();
+      });
+    }
+
+
+    var $lockBox = $('.lock-form--wrap');
+
+    window.addEventListener('resize', function () {
+      wS = $(this).scrollTop();
+      hT = $lockBox.offset().top;
+      if (wS > hT) {
+        $lockBox.addClass('fix-box');
+      }
     });
-  }
-});
+
+    $(window).scroll(function () {
+      wS = $(this).scrollTop();
+      hT = $lockBox.offset().top;
+      hH = $lockBox.outerHeight();
+      if (wS > hT) {
+        $lockBox.addClass('fix-box');
+        $lockBox.css('height', hH + 'px');
+      } else {
+        $lockBox.removeClass('fix-box');
+        $lockBox.css('height', 'inherit');
+      }
+      if (wS > (hT + hH)) {
+        $lockBox.addClass('shrink-box');
+      } else {
+        $lockBox.removeClass('shrink-box');
+      }
+    });
+
+  });
 
 })(jQuery);
