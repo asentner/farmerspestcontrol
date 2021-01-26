b088a837e955bb1f2356378caeb55e9e2e0f77fa
Coalmarch Administrator <coalmarchadministrator@Coalmarchs-iMac.local>
2021-01-26 11:04:19 -0500

    hero template

--- sites/all/themes/farmers/templates/blocks/bean--hero.tpl.php
+++ sites/all/themes/farmers/templates/blocks/bean--hero.tpl.php
@@ -8,7 +8,8 @@
 <?php endif; ?>
     <div class="content-wrap">
     	<div class="content"<?php print $content_attributes; ?>>
-    	  <h2 class="block__title"><?php print $title; ?></h2>
+        <h2 class="block__title"><?php print $title; ?></h2>
+        <?php print render($content['field_title']); ?>
         <?php print render($content['field_subtitle']); ?>
         <?php print render($content['field_body_text']); ?>
         <?php print render($content['field_button']); ?>
